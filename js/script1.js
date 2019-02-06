ddfc3107758af545a555715309e743375ffc44b0
DanielHdez <dhdezleon@gmail.com>
2019-02-06 09:20:45 +0100

    Archivos iniciales

---
+++ js/script1.js
@@ -0,0 +1,29 @@
+
+
+var usuarios= [{
+		"nombre": "usuario1",
+		"apellido": "apell1",
+		"correo": "pepe@gamail.com",
+		"usuaario": "jose",
+		"contraseña": "123456"
+	},
+	{
+		"nombre": "usuario2",
+		"apellido": "apell2",
+		"correo": "pepe2@gamail.com",
+		"usuaario": "Loreno",
+		"contraseña": "654321"
+	}, {
+		"nombre": "usuario3",
+		"apellido": "apell3",
+		"correo": "pepe2@gamail.com",
+		"usuaario": "jose2",
+		"contraseña": "123456"
+	}, {
+		"nombre": "usuario4",
+		"apellido": "apell4",
+		"correo": "pep4@gamail.com",
+		"usuaario": "jose4",
+		"contraseña": "123456"
+	}
+]
